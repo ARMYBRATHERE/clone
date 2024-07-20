2c4cfb9ca4d5666cec03b24a52e4597e4342624a
Adarsh <104964769+ARMYBRATHERE@users.noreply.github.com>
2024-07-20 16:47:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -139,7 +139,7 @@ const App: React.FC = () => {
               <Hamster size={24} className="text-[#d4d4d4]" />
             </div>
             <div>
-              <p className="text-sm">Aviral Kombat</p>
+              <p className="text-sm">Shivam Kombat</p>
             </div>
           </div>
           <div className="flex items-center justify-between space-x-4 mt-1">
